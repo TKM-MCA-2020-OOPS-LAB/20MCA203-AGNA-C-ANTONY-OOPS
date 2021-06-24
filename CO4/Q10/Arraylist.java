871f78f0bfc6db1846aab89714609e788e75e0d4
20MCA203-AGNA-C-ANTONY-OOPS-LAB <agnacantony123@gmail.com>
2021-06-25 01:22:29 +0530

    updateq10

--- CO4/Q10/Arraylist.java
+++ CO4/Q10/Arraylist.java
@@ -14,21 +14,24 @@ public class Arraylist {
 	    System.out.println("Original ArrayList:");
 	    for(String str:n)
 	    	System.out.println(str);
-	    System.out.println("arraylist:");
-	    System.out.println(n);
-	    System.out.println("Element added ArrayList:");
+	    System.out.println(" After addition of an element ArrayList:");
 	    n.add(2,"Venu");
-	    System.out.println(n);
+	    for(String str:n)
+	    	System.out.println(str);
 	    System.out.println("Set new element ArrayList:");
 	    n.set(1,"Swathi");
-	    System.out.println(n);
+	    for(String str:n)
+	    	System.out.println(str);
 	    System.out.println("element removed from ArrayList:");
 	    n.remove("Lilly");
-	    System.out.println(n);
+	    for(String str:n)
+	    	System.out.println(str);
 	    System.out.println("Sorted ArrayList:");
 	    Collections.sort(n);
-	    System.out.println(n);
+	    for(String str:n)
+	    	System.out.println(str);
 	    System.out.println("Size of the ArrayList:"+n.size());
+	    System.out.println("Sreee is present in List:"+n.contains("Sree"));
 	    n.clear();
 	    System.out.println("ArrayList after clear method:"+n);
 	    
